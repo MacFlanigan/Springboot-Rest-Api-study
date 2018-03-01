7a9b587f0996294f27620d94eccefb5f6330a655
nleclerc <ngl.leclerc@gmail.com>
2018-03-01 19:26:07 +0100

    first commit

---
+++ src/main/java/mac/flanigan/StudyRestApplication.java
@@ -0,0 +1,12 @@
+package mac.flanigan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRestApplication.class, args);
+	}
+}
